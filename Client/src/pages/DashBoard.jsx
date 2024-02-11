5f9cf03f623ea597a4631d67d8855b1b0f4ea4f8
Manoj Biswal <manojbiswal521@gmail.com>
2024-02-11 22:11:20 +0530

    Image Upload Functionality

--- Client/src/pages/DashBoard.jsx
+++ Client/src/pages/DashBoard.jsx
@@ -1,5 +1,27 @@
+import { useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
+import DashSideBar from "../components/DashSidebar";
+import DashProfile from "../components/DashProfile";
 const DashBoard = () => {
-  return <div>DashBoard</div>;
+  const location = useLocation();
+  const [tab, setTab] = useState("");
+  useEffect(() => {
+    const urlParams = new URLSearchParams(location.search);
+    const tabFromUrl = urlParams.get("tab");
+    if (tabFromUrl) {
+      setTab(tabFromUrl);
+    }
+  }, [location.search]);
+  return (
+    <div className="min-h-screen flex flex-col md:flex-row">
+      <div className="md:w-56">
+        {/* DashSideBar */}
+        <DashSideBar />
+      </div>
+      {/* DashProfile */}
+      {tab === "profile" && <DashProfile />}
+    </div>
+  );
 };
 
 export default DashBoard;
