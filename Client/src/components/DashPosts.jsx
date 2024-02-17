73b40e1c081c5745962589b2dbfe0720cf1148a1
Manoj Biswal <manojbiswal521@gmail.com>
2024-02-17 23:34:14 +0530

    add show more functionality

--- Client/src/components/DashPosts.jsx
+++ Client/src/components/DashPosts.jsx
@@ -6,7 +6,7 @@ import { Link } from "react-router-dom";
 const DashPosts = () => {
   const { currentUser } = useSelector((state) => state.user);
   const [userPosts, setUserPosts] = useState([]);
-  console.log(userPosts);
+  const [showMore, setShowMore] = useState(true);
   useEffect(() => {
     const fetchPosts = async () => {
       try {
@@ -14,6 +14,9 @@ const DashPosts = () => {
         const data = await res.json();
         if (res.ok) {
           setUserPosts(data.posts);
+          if (data.posts.length < 9) {
+            setShowMore(false);
+          }
         }
       } catch (error) {
         console.log(error.message);
@@ -23,6 +26,24 @@ const DashPosts = () => {
       fetchPosts();
     }
   }, [currentUser._id]);
+
+  const handleShowMore = async () => {
+    const startIndex = userPosts.length;
+    try {
+      const res = await fetch(
+        `/api/post/getposts?userId=${currentUser._id}&startIndex=${startIndex}`
+      );
+      const data = await res.json();
+      if (res.ok) {
+        setUserPosts((prev) => [...prev, ...data.posts]);
+        if (data.posts.length < 9) {
+          setShowMore(false);
+        }
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
   return (
     <div className="table-auto overflow-x-scroll md:mx-auto p-3 scrollbar scrollbar-track-slate-100 scrollbar-thumb-slate-300 dark:scrollbar-track-slate-700 dark:scrollbar-thumb-slate-500">
       {currentUser.isAdmin && userPosts.length > 0 ? (
@@ -83,9 +104,19 @@ const DashPosts = () => {
               ))}
             </Table.Body>
           </Table>
+          {showMore && (
+            <button
+              className="w-full font-medium text-teal-500 self-center text-sm py-7"
+              onClick={handleShowMore}
+            >
+              Show More
+            </button>
+          )}
         </>
       ) : (
-        <p>You have no post yet</p>
+        <p className="font-medium text-red-500 hover:underline cursor-pointer">
+          You have no post yet
+        </p>
       )}
     </div>
   );
