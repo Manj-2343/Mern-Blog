c5ff5c6924d1d28234e67b85e5a3b9a4dafd8d2a
Manoj Biswal <manojbiswal521@gmail.com>
2024-02-19 16:40:22 +0530

    show the coments of the post

--- Api/index.js
+++ Api/index.js
@@ -7,6 +7,7 @@ import cors from "cors";
 import cookieParser from "cookie-parser";
 import postRoutes from "./routes/post.route.js";
 import commentRoutes from "./routes/comment.routes.js";
+
 dotenv.config();
 mongoose
   .connect(process.env.MONGO)
@@ -21,6 +22,7 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 app.use(cookieParser());
+
 
 app.listen(8000, () => {
   console.log("Server is running on port 8000!!!");
--- Api/routes/user.route.js
+++ Api/routes/user.route.js
@@ -2,6 +2,7 @@ import express from "express";
 import { test } from "../controller/controller.js";
 import {
   deleteUser,
+  getUser,
   getUsers,
   signOut,
   updateUser,
@@ -15,4 +16,5 @@ router.put("/update/:userId", verifyToken, updateUser);
 router.delete("/delete/:userId", verifyToken, deleteUser);
 router.post("/signOut", signOut);
 router.get("/getusers", verifyToken, getUsers);
+router.get("/:userId", getUser);
 export default router;
