9a0d1f0553af4945f1a4a983aeda2fe0cb505546
Manoj Biswal <manojbiswal521@gmail.com>
2024-02-25 16:54:00 +0530

    add

--- Client/src/pages/Home.jsx
+++ Client/src/pages/Home.jsx
@@ -35,11 +35,11 @@ export default function Home() {
         <CallToAction />
       </div>
 
-      <div className="max-w-6xl mx-auto p-3 flex flex-col gap-8 py-7">
+      <div className="max-w-12xl mx-auto p-3 flex flex-col gap-3 ml-3 py-7">
         {posts && posts.length > 0 && (
-          <div className="flex flex-col gap-6">
+          <div className="flex flex-col  gap-6">
             <h2 className="text-2xl font-semibold text-center">Recent Posts</h2>
-            <div className="flex flex-wrap gap-4">
+            <div className="grid md:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-2">
               {posts.map((post) => (
                 <PostCard key={post._id} post={post} />
               ))}
